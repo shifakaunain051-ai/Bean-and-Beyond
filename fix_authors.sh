fd6ed576548f1436076a92ca36445515b42bcf4c
Shifa Kaunain <shifakaunain051@gmail.com>
2026-09-11 23:50:42 +0530

    chore: remove redundant fix_authors.sh script

--- fix_authors.sh
+++
@@ -1,4 +0,0 @@
-export GIT_COMMITTER_NAME="Shifa Kaunain"
-export GIT_COMMITTER_EMAIL="shifakaunain051@gmail.com"
-export GIT_AUTHOR_NAME="Shifa Kaunain"
-export GIT_AUTHOR_EMAIL="shifakaunain051@gmail.com"
